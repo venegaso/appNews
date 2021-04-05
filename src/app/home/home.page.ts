696b2e33afa9b441e665f1035aa93c230171ae07
JHOAN MANUEL VENEGAS <venegas_@hotmail.com>
2021-04-05 00:39:33 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { news } from "../../assets/json/news";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  dataNews = news;
+
+  constructor() {
+    
+  }
 
 }
---
+++ src/assets/json/news.ts
@@ -0,0 +1,182 @@
+export const news = [
+    {
+        id: 0,
+        img:'https://static.dw.com/image/57052778_303.png',
+        date: 'Fecha 30.03.2021',
+        title: 'Científicos encuentran posible explicación al misterioso resplandor en el centro de nuestra galaxia',
+        desc: 
+        'El fenómeno llamado Exceso de GeV del Centro Galáctico (GCE) ha sido motivo de muchas preguntas por parte de científicos'+ 
+        'desde su descubrimiento por los físicos Lisa Goodenough y Dan Hooper en 2009. Hasta el día de hoy no se ha podido detectar'+ 
+        'directamente lo que causa este exceso de radiación gamma, una de las luces más energéticas del Universo.',
+        detail: 'Esto es importante',
+        type:'Ciencia',
+        isFav: 'False'
+    },
+
+    {
+        id: 1,
+        img:'https://static.dw.com/image/52649301_303.jpg',
+        date: 'Fecha 03.04.2021',
+        title: 'El retorno de las aspersiones con glifosato planea de nuevo sobre Colombia',
+        desc: 
+        'Hace unas semanas, pobladores de municipios del Pacífico de Colombia se manifestaron ante el'+
+        ' posible retorno de las aspersiones de glifosato en el país. La medida, que pretende erradicar '+
+        'los cultivos ilícitos de coca, inquieta a los campesinos de la zona, que vivieron ya esa experiencia '+
+        'en los años noventa del pasado siglo.',
+        detail: 'Esto es importante',
+        type:'Ecologia',
+        isFav: 'True'
+    },
+
+    {
+        id: 2,
+        img:'https://static.dw.com/image/57067997_303.jpg',
+        date: 'Fecha 31.03.2021',
+        title: 'Mineros de la Edad de Bronce tenían su propio servicio de reparto de comida',
+        desc: 
+        'En un artículo publicado en la revista PLOS ONE, científicos de la Academia Austriaca '+
+        'de Ciencias y de la Universidad de Innsbruck presentaron pruebas que respaldan la idea '+
+        'de que los yacimientos mineros de la Edad de Bronce dependían de fuentes externas para '+
+        'suministrar alimentos preelaborados para mantener a la comunidad y evitar así desplazamientos a media jornada.',
+        type:'Ciencia',
+        isFav: 'True'
+    },
+
+    {
+        id: 3,
+        img:'https://static.dw.com/image/57076380_303.jpg',
+        date: 'Fecha 01.04.2021',
+        title: 'Hallan en la Patagonia nueva especie de dinosaurio carnívoro llamado "el que causa miedo”',
+        desc: 
+            'Un grupo de científicos ha descubierto una nueva especie de dinosaurio carnívoro, bautizada como '+
+            'Llukalkan aliocranianus, que habitaba en la Patagonia, en la Argentina actual, según un estudio '+
+            'publicado en la revista Journal of Vertebrate Paleontology. '+
+            'Hace 80 millones de años, el tiranosaurio dominaba en el hemisferio norte, '+
+            'mientras que Llukalkan aliocranianus, un abelisáurido carnívoro, era uno de los principales depredadores en la Patagonia.',
+        detail: 'Esto es importante',
+        type:'Ciencia',
+        isFav: 'False'
+    },
+
+    {
+        id: 4,
+        img:'https://static.dw.com/image/56891608_303.jpg',
+        date: 'Fecha 17.03.2021',
+        title: 'Bee Mountain, primer santuario de abejas de Guatemala',
+        desc: 
+            'El objetivo es proteger la gran diversidad de abejas existentes en el país. '+
+            'Los integrantes de Beehub Guate van allí donde los llaman y rescatan abejas de '+
+            'todo tipo. En la imagen, abeja cortadora de hojas, llamada así por usar hojas para '+
+            'adecuar su nido dentro de ramas huecas.',
+        detail: 'Esto es importante',
+        type:'Ecologia',
+        isFav: 'False'
+    },
+
+    {
+        id: 5,
+        img:'https://static.dw.com/image/52485341_303.jpg',
+        date: 'Fecha 17.03.2021',
+        title: 'Medioambiente y desarrollo en América Latina: ¿pueden ir de la mano?',
+        desc: 
+            '¿Es posible respetar la naturaleza y generar empleo al mismo tiempo? Prejuicios, '+
+            'creencias cortoplacistas y cálculos mezquinos suelen poner en duda esta posibilidad. '+
+            'Sin embargo, existen muestras de que ambas prácticas no solo pueden coexistir, sino incluso potenciarse entre sí.'+
+            'Costa Rica es un buen ejemplo de esto. El país es pionero en conservación: más de una cuarta parte de su territorio '+
+            'son áreas protegidas o reservas. Y el ecoturismo no para de crecer.',
+        detail: 'Esto es importante',
+        type:'Ecologia',
+        isFav: 'False'
+    },
+
+    {
+        id: 6,
+        img:'https://static.dw.com/image/18489829_303.jpg',
+        date: 'Fecha 31.03.2021',
+        title: 'Premio Memorial Astrid Lindgren para el autor francés Jean-Claude Mourlevat',
+        desc: 
+            'El premio fue creado en 2003 por el Gobierno sueco en honor a Astrid Lindgren y, desde entonces,'+
+            ' se otorga anualmente. Lindgren, autora de clásicos de la literatura infantil como "Pippi Calzaslargas" '+
+            'y "Michel de Lönneberga", murió en 2002.'+
+
+            'Con un premio en metálico de cinco millones de coronas suecas (alrededor de 490.000 euros), este galardón tiene reconocimiento mundial.'+
+            
+            'Hasta ahora, el único ganador alemán del premio fue Wolf Erlbruch en 2017. Sin embargo, los libros infantiles alemanes tienen mucha tradición, '+
+            'como por ejemplo, los hermanos Jacob y Wilhelm Grimm,  en el siglo XIX.',
+        type:'Cultura',
+        isFav: 'False'
+    },
+
+    {
+        id: 7,
+        img:'https://ichef.bbci.co.uk/news/800/cpsprodpb/10925/production/_117077876_declaracionindependencia.jpg',
+        date: 'Fecha 03.04.2021',
+        title: 'Quién fue Otto von Bismarck, el hombre que fundó la Alemania moderna hace 150 años',
+        desc: 
+            'Quien es recordado como el fundador de la Alemania moderna llegó a la política por casualidad en 1847, cuando le pidieron que sustituyera a un miembro del parlamento prusiano que se había enfermado.'+
+            'Hasta entonces, como el segundo hijo de una familia noble menor, había llevado una vida sin mucho propósito, en la que se había ganado el mote del "loco Bismarck"'+
+            ' y una dudosa fama por sus imprudentes bromas y los duelos para defender su honor.'+'Pero cuando, a los 32 años de edad, entró al parlamento, todo cambió para siempre.',
+        type:'Cultura',
+        isFav: 'False'
+    },
+
+    {
+        id: 8,
+        img:'https://ichef.bbci.co.uk/news/800/cpsprodpb/68A2/production/_117768762_promo-05.png',
+        date: 'Fecha 01.04.2021',
+        title: 'Cuáles son los "hábitos ocultos de los genios" según el profesor que enseña el "curso de genialidad" en Yale',
+        desc: 
+            '"El coeficiente intelectual y las notas académicas están sobrevalorados", le dice a BBC Mundo el doctor en musicología estadounidense'+
+            ' Craig Wright, quien lleva más de dos décadas estudiando a las personas más brillantes de la historia y el presente.'+
+            'Wright acaba de publicar el libro The Hidden Habits of Genius: Beyond Talent, IQ, and Grit—Unlocking the Secrets of Greatness '+
+            '("Los hábitos ocultos de los genios: más allá del talento, el coeficiente intelectual y el coraje, desvelando los secretos de la grandeza"), '+
+            'donde detalla 14 rasgos que tienen en común los genios.',
+        type:'Cultura',
+        isFav: 'False'
+    },
+
+    {
+        id: 9,
+        img:'https://ichef.bbci.co.uk/news/800/cpsprodpb/D0F5/production/_117639435_satelite.jpg',
+        date: 'Fecha 03.04.2021',
+        title: 'La costosa carrera por poner satélites en el espacio y quién la domina',
+        desc: 
+            'No solemos pensar en ello muy a menudo pero allá arriba, más lejos que las nubes pero más cerca que la Luna, hay casi 6.000 satélites artificiales dando vueltas a nuestro planeta.'+
+            'Algunos son tan pequeños que los puedes sostener en la mano, otros tan grandes como una ballena y hasta una cancha de fútbol americano.'+
+            'Y si al amanecer o al atardecer, cuando el cielo aún está oscuro pero los rayos del Sol ya alcanzan a iluminarlos, los buscas, quizás tengas la suerte de verlos por unos momentos.'+
+            'Pero si no, durante ese día o esa noche que comienza te servirán, pues son una presencia invisible significativa en nuestra vida cotidiana.'+
+            'De ellos no sólo depende que no te pierdas cuando vas a un lugar que no conoces o que veas al instante un partido de fútbol en una cancha imposiblemente '+
+            'lejana, sino todas las transacciones financieras e incluso la vida de seres humanos en zonas de desastres o de animales en vías de extinción, por nombrar apenas unos ejemplos.',
+        type:'Cultura',
+        isFav: 'False'
+    },
+
+    {
+        id: 10,
+        img:'https://ichef.bbci.co.uk/news/800/cpsprodpb/F14A/production/_117507716_gettyimages-1229993073.jpg',
+        date: 'Fecha 02.04.2021',
+        title: 'Por qué aprender a programar puede cambiar tu vida',
+        desc: 
+            'Independientemente del idioma que elija un programador novato, existen vastos recursos en línea para ayudar a quienes son autodidactas. En su caso, Mungazi dice que freecodecamp.org le fue particularmente útil.'+
+            'Otra opción popular es asistir a unbootcamp (centro de entrenamiento), muy útil para quienes cambian de carrera. Muchos de estos han surgido en los últimos años, ofreciendo cursos intensivos diseñados para equipar '+
+            'a los participantes con las habilidades necesarias para obtener ese primer trabajo crucial.',
+        type:'Cultura',
+        isFav: 'False'
+    },
+
+    {
+        id: 11,
+        img:'https://ichef.bbci.co.uk/news/800/cpsprodpb/BAF7/production/_117636874_laptop.jpg',
+        date: 'Fecha 23.03.2021',
+        title: '¿Es mejor tener la laptop enchufada todo el tiempo o usar la batería?: esto es lo que dicen los expertos',
+        desc: 
+            'Las laptops de Lenovo y otras marcas "usan sensores y lógica de control para garantizar que la batería no se sobrecargue ni se sobrecaliente", explica.'+
+            'Sin embargo, "mantener una batería al 100% todo el tiempo reducirá ligeramente su vida útil".'+
+            'Su colega Phil Jakes, director de Tecnología Estratégica e ingeniero principal de Lenovo, '+
+            'coincide con él: "Con la química de mayor densidad de energía adoptada en los últimos años, hemos descubierto que las baterías se'+
+            ' degradan mucho más rápido si se mantienen completamente cargadas, especialmente a temperaturas más altas", dice a BBC Mundo.',
+        type:'Cultura',
+        isFav: 'False'
+    },
+
+];
